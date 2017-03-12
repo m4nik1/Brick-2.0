7be26e11ff4d870b8b334e7142a0df672289423a
m4nik1 <maniksoomro@gmail>
2017-03-11 21:05:38 -0500

    update 3.1.2

--- src/org/usfirst/frc/team558/robot/commands/GearIn.java
+++ src/org/usfirst/frc/team558/robot/commands/GearIn.java
@@ -11,7 +11,7 @@ public class GearIn extends Command {
 
     public GearIn() {
         // Use requires() here to declare subsystem dependencies
-    	requires(Robot.speedGearIntake);
+    	requires(Robot.gearIntakeMotors);
     }
 
     // Called just before this Command runs the first time
@@ -21,7 +21,7 @@ public class GearIn extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    	Robot.speedGearIntake.SetGearIntake(1);
+    	Robot.gearIntakeMotors.SetGearIntake(1);
     	
     }
 
